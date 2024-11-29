0718539cf6436bc68881b99a8d1ac0da2b223e4f
Anish Neunaha <anishneunaha1@gmail.com>
2024-11-29 13:56:52 +0530

    created helper file

--- src/helper.py
+++ src/helper.py
@@ -1,3 +1,32 @@
 import os
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+from langchain.vectorstores import Pinecone
+from langchain_openai import OpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+#Extract Data From the PDF File
+def load_pdf_file(data):
+    loader = DirectoryLoader(data,
+                             glob = "*.pdf",
+                             loader_cls = PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+
+#Split the data into Chunks
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap =20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+#Download the embeddigns from HuggingFace
+def download_hugging_face_embeddings():
+    embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+    return embeddings
