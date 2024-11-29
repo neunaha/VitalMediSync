4bed2e93c297c02b675b7e39e5b79801b5e58bb1
Anish Neunaha <anishneunaha1@gmail.com>
2024-11-29 12:02:45 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "VitalMediSync",
+    version = "0.0.0",
+    author = "Anish Neunaha",
+    author_email = "aneunaha@my.ggu.edu",
+    packages = find_packages(),
+    install_requires = []
+)
